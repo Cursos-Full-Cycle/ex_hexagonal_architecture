5daaf0ca5cad435f9eec13964783daf62d3aa405
Fabio de Stefani <fabiostefani@gmail.com>
2023-03-25 05:57:18 -0300

    negócio

--- application/product.go
+++ application/product.go
@@ -1,9 +1,14 @@
 package application
 
 import (
-	"errors"	
+	"errors"
+	"github.com/asaskevich/govalidator"	
+	uuid "github.com/satori/go.uuid"
 )
 
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type ProductInterface interface {
 	IsValid() (bool, error)
@@ -12,7 +17,27 @@ type ProductInterface interface {
 	GetId() string
 	GetName() string
 	GetStatus() string
-	GetPricec() float64
+	GetPrice() float64
+}
+
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductReaderInterface interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriterInterface interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReaderInterface
+	ProductWriterInterface
 }
 
 const (
@@ -21,14 +46,39 @@ const (
 )
 
 type Product struct {
-	Id     string 
-	Name   string 
-	Status string 
-	Price  float64
+	ID     string `valid:"uuidv4"`
+	Name   string `valid:"required"` 
+	Status string `valid:"required"` 
+	Price  float64 `valid:"float,optional"`
+}
+
+func NewProduct() *Product {
+	product := Product{
+		ID:     uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+	return &product
 }
 
 func (p *Product) IsValid() (bool, error) {
-	return true, nil
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+	
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("the status must be enabled or disabled")
+	}
+	
+	if p.Price < 0 {
+		return false, errors.New("the price must be greater or equal zero")
+	}
+	
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return false, err
+	}
+	
+	return true, nil	
 }
 
 func (p *Product) Enable() error {
@@ -40,16 +90,16 @@ func (p *Product) Enable() error {
 }
 
 func (p *Product) Disable() error {
-	if p.Status == DISABLED {
-		return errors.New("product already disabled")
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
 	}
 	
-	p.Status = DISABLED
-	return nil
+	return errors.New("the price must be zero to disable the product")	
 }
 
 func (p *Product) GetId() string {
-	return p.Id
+	return p.ID
 }
 
 func (p *Product) GetName() string {
--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"github.com/stretchr/testify/require"
 	"go_hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestProduct_Enabled(t *testing.T) {
@@ -19,3 +20,64 @@ func TestProduct_Enabled(t *testing.T) {
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())	
 }
 
+func TestProduct_Disabled(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Price = 0
+	product.Status = application.ENABLED	
+	err := product.Disable()	
+	require.Nil(t, err)	
+	
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero to disable the product", err.Error())	
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Price = 10
+	product.Status = application.DISABLED
+	product.ID = uuid.NewV4().String()
+	
+	_, err := product.IsValid()
+	require.Nil(t, err)
+	
+	product.Status = "invalid"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+	
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+	
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+	
+}
+
+func TestProduct_GetId(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	require.Equal(t, product.ID, product.GetId())
+}
+
+func TestProduct_GetName(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	require.Equal(t, product.Name, product.GetName())
+}
+
+func TestProduct_GetStatus(t *testing.T) {
+	product := application.Product{}
+	product.Status = application.ENABLED
+	require.Equal(t, product.Status, product.GetStatus())
+}
+
+func TestProduct_GetPrice(t *testing.T) {
+	product := application.Product{}
+	product.Price = 10
+	require.Equal(t, product.Price, product.GetPrice())
+	}	
+	
